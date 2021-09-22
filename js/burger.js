a8af775a51bfb42b72580b48e271f47b3929d5f3
Santi Code <49213563+codewithsanti@users.noreply.github.com>
2021-09-22 10:23:43 -0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,5 @@
+let boton_menu = document.getElementById('boton_menu');
+let menu = document.getElementById('menu');
+boton_menu.addEventListener('click', function() {
+    menu.classList.toggle('aparecer');
+});
